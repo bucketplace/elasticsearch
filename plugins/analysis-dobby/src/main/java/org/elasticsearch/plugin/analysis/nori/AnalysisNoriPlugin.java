57547bb5efb5efbe3072cdf6c9039820fd4e9083
oody-donggyu <dg.oh@bucketplace.net>
2022-10-23 17:54:28 +0900

    Dobby 이름 설정 및 dependency 파일 경로 변경

--- plugins/analysis-dobby/src/main/java/org/elasticsearch/plugin/analysis/nori/AnalysisNoriPlugin.java
+++ plugins/analysis-dobby/src/main/java/org/elasticsearch/plugin/analysis/nori/AnalysisNoriPlugin.java
@@ -30,19 +30,19 @@ public class AnalysisNoriPlugin extends Plugin implements AnalysisPlugin {
     @Override
     public Map<String, AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
         Map<String, AnalysisProvider<TokenFilterFactory>> extra = new HashMap<>();
-        extra.put("nori_part_of_speech", NoriPartOfSpeechStopFilterFactory::new);
-        extra.put("nori_readingform", NoriReadingFormFilterFactory::new);
-        extra.put("nori_number", NoriNumberFilterFactory::new);
+        extra.put("dobby_part_of_speech", NoriPartOfSpeechStopFilterFactory::new);
+        extra.put("dobby_readingform", NoriReadingFormFilterFactory::new);
+        extra.put("dobby_number", NoriNumberFilterFactory::new);
         return extra;
     }
 
     @Override
     public Map<String, AnalysisProvider<TokenizerFactory>> getTokenizers() {
-        return singletonMap("nori_tokenizer", NoriTokenizerFactory::new);
+        return singletonMap("dobby_tokenizer", NoriTokenizerFactory::new);
     }
 
     @Override
     public Map<String, AnalysisProvider<AnalyzerProvider<? extends Analyzer>>> getAnalyzers() {
-        return singletonMap("nori", NoriAnalyzerProvider::new);
+        return singletonMap("dobby", NoriAnalyzerProvider::new);
     }
 }
